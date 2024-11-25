41ccb8c0e7f4eeead80bf2830ad6c87662f6940e
laur021 <laurence.bacolor@gmail.com>
2024-11-25 11:59:50 +0800

    fix launch port

--- CNCSwebApiProject/Controllers/ActivityLogController.cs
+++ CNCSwebApiProject/Controllers/ActivityLogController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace CNCSapi.Controllers
 {
     [EnableCors("AllowOrigin")]
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class ActivityLogController(IActivityLogRepository _activityLogRepository) : ControllerBase
     {
