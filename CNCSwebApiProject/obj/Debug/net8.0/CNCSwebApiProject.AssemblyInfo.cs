ad93d2675d82c79279ff4937fff3caeac3f8e6d2
Alvin V. Rebulado <rebuladovince31@gmail.com>
2024-12-02 09:17:35 +0800

    12022024 0917

--- CNCSwebApiProject/obj/Debug/net8.0/CNCSwebApiProject.AssemblyInfo.cs
+++ CNCSwebApiProject/obj/Debug/net8.0/CNCSwebApiProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CNCSwebApiProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2ae76720d238c8ca23ffeaffe1acb1419f2ab4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0b4d734e17992e25940941dda9745d0ffc48dac")]
 [assembly: System.Reflection.AssemblyProductAttribute("CNCSwebApiProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CNCSwebApiProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
